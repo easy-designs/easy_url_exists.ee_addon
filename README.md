57e63ee8ff083670779b12767289c67765a50516
aarongustafson <aaron@easy-designs.net>
2014-03-21 15:05:42 -0400

    Tweaked the instructions and added "yes" status for all 200 & 300 status codes

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 Easy URL Exists EE Plugin
 ========================
 
-This script allows you to check to see if a resource URL exists. It returns a value of yes or no. A common use of this plugin would be to check if the URL exists within a conditional to set up an alternative if the resource comes back with a 404.
+This script allows you to check to see if a resource URL exists. It returns a value of "yes" or "no". A common use of this plugin would be to check if an external resource exists within a conditional to set up an alternative if the resource comes back with a 404.
 
-Here’s an example of setting a variable using the plugin syntax
+Here’s an example of setting a variable using the plugin syntax:
 
 	{preload_replace:has_photo="{exp:easy_file_exists url='{photo}'}"}
 
-Here’s an example of how to use that variable in the template:
+Then, later in the template:
 
-	<img src="{if has_photo='yes'}{photo}{if:else}{default_avatar}{/if}" class="user-avatar" />
+	<img src="{if has_photo='yes'}{photo}{if:else}{default_avatar}{/if}" class="user-avatar" alt="" />
+	
+This is extremely useful when the file in question is an external resource as a simple `{if photo}` will evaluate as "true" if `{photo}` has a value, but the conditional does not check against the actual availability of the linked resource.
--- system/expressionengine/third_party/easy_url_exists/pi.easy_url_exists.php
+++ system/expressionengine/third_party/easy_url_exists/pi.easy_url_exists.php
@@ -31,33 +31,39 @@ class Easy_url_exists {
 	 */
 	function __construct( $url = '' )
 	{
+		# Get the URL
 		$url = ee()->TMPL->fetch_param( 'url', $url );
+		
+		# Pessimism
 		$this->return_data = 'no';
-		if ( !empty($url) )
+		
+		# Let’s test it
+		if ( !empty( $url ) &&
+		 	 function_exists( 'curl_exec' ) )
 		{
 			# init cURL
 			$ch = curl_init();
 			
 			# setup request
-			curl_setopt($ch, CURLOPT_URL, $url);
+			curl_setopt( $ch, CURLOPT_URL, $url );
 			curl_setopt( $ch, CURLOPT_HEADER, true );
 			
 			# make request
 			curl_exec($ch);
 			
 			# get a response
-			$response = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+			$response = curl_getinfo( $ch, CURLINFO_HTTP_CODE );
 			
 			curl_close( $ch );
 			
-			if( $response == 200 )
+			if ( $response >= 200 &&
+			 	 $response < 400 )
 			{
 				$this->return_data = 'yes';
 			}
 		}
 	
 		return $this->return_data;
-		
 		
 	} # end Easy_url_exists constructor
 	
@@ -68,16 +74,17 @@ class Easy_url_exists {
 	function usage()
 	{
 		ob_start(); ?>
-This script allows you to check to see if a resource URL exists. It returns a value of yes or no. A common use of this plugin would be to check if the URL exists within a conditional to set up an alternative if the resource comes back with a 404.
+This script allows you to check to see if a resource URL exists. It returns a value of "yes" or "no". A common use of this plugin would be to check if an external resource exists within a conditional to set up an alternative if the resource comes back with a 404.
 
-Here’s an example:
+Here’s an example of setting a variable using the plugin syntax:
 
-// Setting a variable using the plugin syntax
 {preload_replace:has_photo="{exp:easy_file_exists url='{photo}'}"}
 
-<img src="{if has_photo='yes'}{photo}{if:else}{default_avatar}{/if}" class="user-avatar" />
+Then, later in the template:
 
+<img src="{if has_photo='yes'}{photo}{if:else}{default_avatar}{/if}" class="user-avatar" alt="" />
 
+This is extremely useful when the file in question is an external resource as a simple `{if photo}` will evaluate as "true" if `{photo}` has a value, but the conditional does not check against the actual availability of the linked resource.
 <?php
 		$buffer = ob_get_contents();
 		ob_end_clean();
